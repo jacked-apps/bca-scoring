eb138bfd2ff2d339c1d9416b5b588f58ee8ddce2
H Edward Poplet <56243827+shodbyed@users.noreply.github.com>
2023-05-17 00:13:53 -0700

    messed around with the header on my test page. creates a back button that goes to Second Page

--- src/screens/TestScreen.js
+++ src/screens/TestScreen.js
@@ -1,10 +1,11 @@
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, TouchableOpacity } from 'react-native';
 import { postEditRoster } from '../constants/posts';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useLayoutEffect, useState } from 'react';
 import { Text, Button } from 'react-native-paper';
 import LoadingScreen from '../components/LoadingScreen';
 import { URL } from '../constants/url';
 import { SelectList } from 'react-native-dropdown-select-list';
+import { Ionicons } from '@expo/vector-icons';
 
 const TestScreen = ({ route, navigation }) => {
   const { table, home } = route.params;
@@ -20,6 +21,25 @@ const TestScreen = ({ route, navigation }) => {
     { key: '2', value: 'two' },
     { key: '3', value: 'three' },
   ];
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: 'CHECK ME OUT',
+      headerStyle: {
+        backgroundColor: 'lightblue',
+      },
+      headerTintColor: 'green',
+      headerLeft: () => (
+        <TouchableOpacity
+          style={{ marginLeft: 10 }}
+          onPress={() => navigation.navigate('Second Page', { table, home })}
+        >
+          <Ionicons name='arrow-back' size={24} color='white' />
+        </TouchableOpacity>
+      ),
+    });
+  }, [navigation]);
+
   const handleSelect = name => {
     console.log('whatever');
     setSelected(name);
