755d491c78b0b8cf10f50f56b3412d18476b7d58
H Edward Poplet <56243827+shodbyed@users.noreply.github.com>
2023-09-12 15:35:09 -0700

    adds some styles and adds exempt emails

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { StyleSheet, Text, StatusBar } from 'react-native';
+import { View, Text, StatusBar } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import 'react-native-gesture-handler';
 import { Navigator } from './src/navigations/Navigator';
@@ -9,9 +9,11 @@ import { styles } from './src/constants/StyleMaster';
 export default function App() {
   return (
     <SafeAreaView style={styles.safeArea}>
-      <NavigationContainer>
-        <Navigator />
-      </NavigationContainer>
+      <View style={styles.fullContainer}>
+        <NavigationContainer>
+          <Navigator />
+        </NavigationContainer>
+      </View>
     </SafeAreaView>
   );
 }
